209ca1558129a1624d7681c70d439cc7c3e6283d
Ben Prat <65628677+benjaminprat@users.noreply.github.com>
2021-06-10 15:10:05 -0600

    update resume link

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,38 +1,36 @@
-import body from '../styles/Home.module.scss'
-import styles from '../styles/Contact.module.scss'
+import body from "../styles/Home.module.scss";
+import styles from "../styles/Contact.module.scss";
 
-import Tarot from '../components/Tarot'
+import Tarot from "../components/Tarot";
 
 export default function Contact() {
   return (
     <main className={body.main}>
-
       <div className={styles.container}>
         <Tarot
-          link='https://github.com/benjaminprat'
-          img='hat'
+          link="https://github.com/benjaminprat"
+          img="hat"
           height={150}
           width={150}
-          name='Github'
-          />
+          name="Github"
+        />
 
         <Tarot
-          link='https://www.linkedin.com/in/benjaminprat/'
-          img='plant bottle'
+          link="https://www.linkedin.com/in/benjaminprat/"
+          img="plant bottle"
           height={200}
           width={150}
-          name='LinkedIn'
-          />
+          name="LinkedIn"
+        />
 
         <Tarot
-          link='/Alia Peterson Resume 2021.pdf'
-          img='moon'
+          link="https://docs.google.com/document/d/e/2PACX-1vTkoFLVETGcEQyOWw5a7r9JZDvuALwuEMr78XdlNVBk5Jqrwq5hlSTS6FqTy7WTAOvIlnDqN-C_xiXD/pub"
+          img="moon"
           height={150}
           width={150}
-          name='Resume'
-          />
-
+          name="Resume"
+        />
       </div>
     </main>
-  )
+  );
 }
