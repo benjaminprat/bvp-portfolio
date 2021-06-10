c29a3b6deb7381c5354263b20262cd1b509254d7
Ben Prat <65628677+benjaminprat@users.noreply.github.com>
2021-06-10 14:59:37 -0600

    Update information.js

--- data/information.js
+++ data/information.js
@@ -1,17 +1,20 @@
 const info = {
   summary: [
-    'I transitioned to programming because it fulfilled my desire to have a career in which I could always continue to learn, problem solve, and contribute to something larger than myself, which I hope benefits others. Ultimately, I believes programming allows you to be creative, explorative, and help others.',
-    ' I believe that programming gives people a tremendous toolset for building solutions to problems and touches every business, government agency, and home. I will continue to weave a deep commitment to service leveraging programming to make life easier for others. '
-  ],
-  current: ['VueX', 'Bootstrap'],
-  proficient: {
-    Frameworks: 'React.js, Vue.js, Ruby on Rails',
-    Languages: 'Javascript, HTML, CSS & SCSS, Ruby', 
-    Tools: 'Next.js, API Consumption, API Creation w Restful Routes, Github Projects, Version Control, Chart.js, PostgreSQL',
-    Testing: 'Cypress, Jest, Mocha, Chai',
-    Deployment: 'Heroku, Vercel',
-    'CI/CD': 'TravisCI, CircleCI'
-  }
-}
+    "I believe programming gives people a tremendous toolset for building solutions to problems and touches every business, government agency, and home. I will continue to weave my deep commitment to service leveraging programming to make life easier for others.",
 
-export default info
+    "I transitioned to programming because it fulfilled my desire to have a career in which I could always continue to learn, problem solve, and contribute to something larger than myself.",
+  ],
+  current: ["VueX", "Bootstrap"],
+  proficient: {
+    Frameworks: "React.js, Vue.js, Ruby on Rails",
+    Languages: "Javascript, Ruby, HTML, CSS & SCSS",
+    Tools:
+      "Next.js, Chart.js, API Consumption, API Creation w Restful Routes, Github Projects, Version Control, PostgreSQL",
+    Testing: "Cypress, Jest, Mocha, Chai",
+    Deployment: "Heroku, Vercel",
+    "CI/CD": "TravisCI, CircleCI",
+  },
+};
+
+export default info;
+
